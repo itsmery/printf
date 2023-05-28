7d8697082033a582b8edb819546a801602d1a2d4
itsmery <gl.mimi2020@gmail.com>
2023-05-28 10:20:16 -0700

    char function

--- function_char.c
+++ function_char.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * handle_char - handles the case c specifier
+ * function_char - handles the case c specifier
  * @args: args list
  * Return: the number of chars printed
  */
-int handle_char(va_list args)
+int function_char(va_list args)
 {
 	char c = va_arg(args, int);
 
